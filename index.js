57faf8766b6a888b2d171e6258061e35f17af60b
Maira Bello <maira.araujo@liferay.com>
2015-04-01 15:07:57 -0300

    Adds logic to extract template information from soy file

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
+var merge = require('merge');
+var tunic = require('tunic');
+
 function extractNamespace(text) {
   return /{namespace (.*)}/.exec(text)[1];
 }
 
+function extractParams(text) {
+  var params = [];
+  var paramRegex = /{@param \s*(\S*)\s*:\s*(\S*)\s*\/}/g;
+  var currentMatch = paramRegex.exec(text);
+  while (currentMatch) {
+    params.push({
+      name: currentMatch[1],
+      type: currentMatch[2]
+    })
+    currentMatch = paramRegex.exec(text);
+  }
+  return params;
+}
+
+function extractTemplateInfo(text) {
+  var info = {};
+  var match = /{template (.*)}/.exec(text);
+  if (match){
+    info.name = match[1].substr(1);
+  } else {
+    var regex = new RegExp('{deltemplate (\\S+)\\s*(variant="\'(\\w+)\'")?\\s*}');
+    match = regex.exec(text);
+    if (match) {
+      info.deltemplate = true;
+      info.name = match[1];
+      info.variant = match[3];
+    }
+  }
+  return info;
+}
+
+function extractTemplates(templates, blocks, block, index) {
+  var code = blocks[index + 1];
+  if (block.type === 'Comment' && code && code.type === 'Code') {
+    var templateInfo = extractTemplateInfo(code.contents);
+    if (templateInfo) {
+      var info = merge(
+        {
+          contents: code.contents,
+          docTags: block.tags,
+          params: getAllParams(extractParams(code.contents), block.tags)
+        },
+        templateInfo
+      );
+      templates.push(info);
+    }
+  }
+}
+
+function getAllParams(extractedParams, docTags) {
+  var params = extractedParams.concat();
+  docTags.forEach(function(tag) {
+    if (tag.tag === 'param') {
+      params.push({
+        name: tag.name,
+        type: 'any'
+      });
+    }
+  });
+  return params;
+}
+
 function soyparser(text) {
   var parsed = {
-    namespace: extractNamespace(text)
+    namespace: extractNamespace(text),
+    templates: []
   };
+  var ast = tunic().parse(text);
+  ast.blocks.forEach(extractTemplates.bind(null, parsed.templates, ast.blocks));
   return parsed;
 };
 
--- test/test.js
+++ test/test.js
@@ -1,10 +1,101 @@
 var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
 var soyparser = require('../index');
 
+var testSoyContents = fs.readFileSync(path.join(__dirname, 'assets/test.soy'));
+
 module.exports = {
-	testNamespace: function(test) {
-		var parsed = soyparser('{namespace My.Namespace}');
-		assert.strictEqual('My.Namespace', parsed.namespace);
-		test.done();
-	}
+  testNamespace: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual('My.Namespace', parsed.namespace);
+    test.done();
+  },
+
+  testTemplateCount: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual(4, parsed.templates.length);
+    test.done();
+  },
+
+  testTemplateNames: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual('hello', parsed.templates[0].name);
+    assert.strictEqual('age', parsed.templates[1].name);
+    assert.strictEqual('Del', parsed.templates[2].name);
+    assert.strictEqual('Del', parsed.templates[3].name);
+    test.done();
+  },
+
+  testDelTemplates: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.ok(!parsed.templates[0].deltemplate);
+    assert.ok(!parsed.templates[1].deltemplate);
+    assert.ok(parsed.templates[2].deltemplate);
+    assert.ok(parsed.templates[3].deltemplate);
+    test.done();
+  },
+
+  testDelTemplateVariants: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.ok(!parsed.templates[0].variant);
+    assert.ok(!parsed.templates[1].variant);
+    assert.ok(!parsed.templates[2].variant);
+    assert.strictEqual('negation', parsed.templates[3].variant);
+    test.done();
+  },
+
+  testDocTags: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual(1, parsed.templates[0].docTags.length);
+    assert.strictEqual(0, parsed.templates[1].docTags.length);
+    assert.strictEqual(1, parsed.templates[2].docTags.length);
+    assert.strictEqual(1, parsed.templates[3].docTags.length);
+    test.done();
+  },
+
+  testDocTagTags: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual('param', parsed.templates[0].docTags[0].tag);
+    assert.strictEqual('param', parsed.templates[2].docTags[0].tag);
+    assert.strictEqual('param', parsed.templates[3].docTags[0].tag);
+    test.done();
+  },
+
+  testDocTagNames: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual('firstName', parsed.templates[0].docTags[0].name);
+    assert.strictEqual('id', parsed.templates[2].docTags[0].name);
+    assert.strictEqual('id', parsed.templates[3].docTags[0].name);
+    test.done();
+  },
+
+  testParams: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual(2, parsed.templates[0].params.length);
+    assert.strictEqual(1, parsed.templates[1].params.length);
+    assert.strictEqual(1, parsed.templates[2].params.length);
+    assert.strictEqual(1, parsed.templates[3].params.length);
+    test.done();
+  },
+
+  testParamNames: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual('lastName', parsed.templates[0].params[0].name);
+    assert.strictEqual('firstName', parsed.templates[0].params[1].name);
+    assert.strictEqual('age', parsed.templates[1].params[0].name);
+    assert.strictEqual('id', parsed.templates[2].params[0].name);
+    assert.strictEqual('id', parsed.templates[3].params[0].name);
+    test.done();
+  },
+
+  testParamTypes: function(test) {
+    var parsed = soyparser(testSoyContents);
+    assert.strictEqual('string', parsed.templates[0].params[0].type);
+    assert.strictEqual('any', parsed.templates[0].params[1].type);
+    assert.strictEqual('number', parsed.templates[1].params[0].type);
+    assert.strictEqual('any', parsed.templates[2].params[0].type);
+    assert.strictEqual('any', parsed.templates[3].params[0].type);
+    test.done();
+  }
 };
